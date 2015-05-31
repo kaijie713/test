b0bcc6d76593b87a1161856585549736fe6c6f08
kaijie713 <kaijie713@qq.com>
2015-05-31 15:47:03 +0800

    s

--- themes/sohu/views/layouts/main.php
+++ themes/sohu/views/layouts/main.php
@@ -282,6 +282,11 @@
 		</div>
 	</div>
 </div>
+
+
+<div class="row-fluid">
+  <div id="footer" class="span12">Copyright © 2014 Sohu.com Inc. All Rights Reserved. 搜狐公司 <a href="http://house.focus.cn/aboutus/banquanshengming.html" target="_blank" rel="nofollow">版权所有</a></div>
+</div>
 <div id="modal" class="modal"></div>
 <?php //$this->renderPartial('/layouts/script_boot');?>
 <?php include('script_boot.php');?>
